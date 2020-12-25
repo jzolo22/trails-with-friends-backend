058969247586b02a3dcaf4d45638b6adde420572
jonnyhak <jonnyhak@gmail.com>
2020-12-25 17:11:41 -0500

    backend auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create] 
 
-    def index
-        users = User.all 
-        render json: users
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted 
+    end
+    
+    # def index
+    #     users = User.all 
+    #     render json: users
+    # end
+
+    # def show
+    #     user = User.find(params[:id])
+    #     render json: user
+    # end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created 
+        else    
+            render json: { error: 'failed to create user' }, status: :not_acceptable 
+        end
     end
 
-    def show
-        user = User.find(params[:id])
-        render json: user
+    private
+    def user_params
+        params.require(:user).permit(:username, :password, :city, :age, :name)
     end
     
 end
