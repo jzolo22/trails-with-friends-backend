31f84cce115fc3c47bfdfec985a8a48a29f7a6f7
jonnyhak <jonnyhak@gmail.com>
2020-12-25 16:13:08 -0500

    bcrypt and password_digest added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :user_trails 
-    has_many :trails, through: :user_trails 
+    has_many :trails, through: :user_trails
+    
+    has_secure_password 
+    validates :username, uniqueness: {case_sensitive: false}
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_235833) do
+ActiveRecord::Schema.define(version: 2020_12_25_210920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_12_24_235833) do
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
