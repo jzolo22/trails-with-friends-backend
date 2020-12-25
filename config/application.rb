5a6ee4a70af450acf318ad617655e63df89adc3e
jzolo22 <jzolotarev@gmail.com>
2020-12-24 19:47:15 -0500

    just in case

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,16 @@ Bundler.require(*Rails.groups)
 
 module TrailsWithFriends
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
@@ -35,3 +45,4 @@ module TrailsWithFriends
     config.api_only = true
   end
 end
+
