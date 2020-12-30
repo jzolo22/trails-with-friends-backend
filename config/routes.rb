113f0507366898b459f9b6b422ec0a82be52105e
jonnyhak <jonnyhak@gmail.com>
2020-12-29 21:01:58 -0500

    before action on application controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :user_trails
   resources :trails
   resources :users
+  
+  post '/login', to: 'auth#create'
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
