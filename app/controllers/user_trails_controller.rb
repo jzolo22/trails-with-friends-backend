e252698968b57629ee53b2758cd8989bbafd4849
jonnyhak <jonnyhak@gmail.com>
2020-12-31 18:00:11 -0500

    userTrails controller

--- app/controllers/user_trails_controller.rb
+++ app/controllers/user_trails_controller.rb
@@ -1,5 +1,10 @@
 class UserTrailsController < ApplicationController
 
-    
+    skip_before_action :authorized, only: [:index]
+
+    def index 
+        user_trails = UserTrail.all
+        render json: user_trails
+    end
 
 end
