92bc48c94c251df0cd74e2d71ac75c7ddbd3d1bb
jzolo22 <jzolotarev@gmail.com>
2021-01-06 21:20:56 -0500

    users added

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,32 @@ Trail.destroy_all
 UserTrail.destroy_all 
 
 
-tashawn = User.create!(name: "Tashawn", username: "TW", age: 30, city: "LA", password: "password123", image_url: "https://media-exp1.licdn.com/dms/image/C4E03AQG2OsxO3Osdgw/profile-displayphoto-shrink_200_200/0/1579100870890?e=1615420800&v=beta&t=o8Xfii1KntBbLvWPgRGqW2CFukfgh5JO_r2IaYZ_gqU")
+tashawn = User.create!(name: "Tashawn", username: "TW", age: 30, city: "Los Angeles", password: "password123", image_url: "https://media-exp1.licdn.com/dms/image/C4E03AQG2OsxO3Osdgw/profile-displayphoto-shrink_200_200/0/1579100870890?e=1615420800&v=beta&t=o8Xfii1KntBbLvWPgRGqW2CFukfgh5JO_r2IaYZ_gqU")
+
+bg = User.create!(name: "Bryan G", username: "bryang", age: 30, city: "Queens", password: "password123")
+cc = User.create!(name: "Chris", username: "chrisc", age: 30, city: "Schenectady NY", password: "password123")
+dl = User.create!(name: "Demetrio", username: "demetriol", age: 30, city: "Brooklyn NY", password: "password123")
+es = User.create!(name: "Eitan", username: "eitancs", age: 30, city: "Cold Spring NY", password: "password123")
+ja = User.create!(name: "James", username: "jamesa", age: 30, city: "Brooklyn NY", password: "password123")
+jg = User.create!(name: "Javaughn", username: "javaughng", age: 19, city: "Brooklyn NY", password: "password123")
+sg = User.create!(name: "Shevaughn", username: "shevaughng", age: 19, city: "Brooklyn NY", password: "password123")
+jk = User.create!(name: "Joe", username: "joek", age: 30, city: "Brooklyn NY", password: "password123")
+je = User.create!(name: "John", username: "johne", age: 19, city: "Brooklyn NY", password: "password123")
+jp = User.create!(name: "Jomarie", username: "jomariep", age: 30, city: "Edgewater NJ", password: "password123")
+kg = User.create!(name: "Karina", username: "karinag", age: 30, city: "Brooklyn NY", password: "password123")
+kh = User.create!(name: "Kevin", username: "kevinh", age: 30, city: "Brooklyn NY", password: "password123")
+lm = User.create!(name: "Lee", username: "leem", age: 30, city: "Brooklyn NY", password: "password123")
+mk = User.create!(name: "Mary", username: "maryk", age: 30, city: "New Jersey", password: "password123")
+pa = User.create!(name: "Patricia", username: "patriciaa", age: 30, city: "Brooklyn NY", password: "password123")
+rr = User.create!(name: "Rebecca", username: "rebeccar", age: 30, city: "Saratoga FL", password: "password123")
+rt = User.create!(name: "Roman", username: "romant", age: 30, city: "Brooklyn NY", password: "password123")
+sb = User.create!(name: "Sam", username: "samdb", age: 30, city: "Brooklyn NY", password: "password123")
+sd = User.create!(name: "Sean D", username: "seand", age: 30, city: "Brooklyn NY", password: "password123")
+sp = User.create!(name: "Sean P", username: "seanp", age: 30, city: "Brooklyn NY", password: "password123")
+st = User.create!(name: "Shelby", username: "shelbyp", age: 30, city: "Brooklyn NY", password: "password123")
+sw = User.create!(name: "Steven", username: "stevenw", age: 30, city: "Brooklyn NY", password: "password123")
+vt = User.create!(name: "Victor", username: "victort", age: 30, city: "Bronx NY", password: "password123")
+wn = User.create!(name: "Warren", username: "warrenn", age: 30, city: "Brooklyn NY", password: "password123")
 
 alpine = Trail.create!(name: "Alpine", length: 4, location: "NJ", duration: 180, difficulty: "easy", image_url: "https://www.njpalisades.org/images/stairsDown.jpg")
 
@@ -22,6 +47,8 @@ cp = Trail.create(name: "Central Park Trail", length: 4, location: "New York, NY
 pp = Trail.create(name: "Prospect Park Loop", length: 4, location: "Brooklyn, NY", duration: 95, difficulty: "easy", image_url: "https://d21xlh2maitm24.cloudfront.net/nyc/propsect-park-summer.jpg?mtime=20161129143144")
 
 si = Trail.create(name: "Staten Island Greenbelt Trail", length: 3, location: "Brooklyn, NY", duration: 79, difficulty: "easy", image_url: "https://thebannercsi.files.wordpress.com/2018/11/hiking.jpg")
+
+
 
 
 tashawnAlpine = UserTrail.create(user_id: User.first.id, trail_id: Trail.first.id, date: Date.today)
